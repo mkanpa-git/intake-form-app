67f47cd819c0ca7b938b08d31603cbc42cf1fbdf
mkanpa-git <79177729+mkanpa-git@users.noreply.github.com>
2025-06-05 01:19:51 -0400

    Update RadioGroup options and props

--- test-form/src/components/shared/RadioGroup/RadioGroup.jsx
+++ test-form/src/components/shared/RadioGroup/RadioGroup.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import styles from './RadioGroup.module.css';
 
-export default function RadioGroup({ id, label, options = [], ...props }) {
+export default function RadioGroup({
+  id,
+  label,
+  options = [],
+  value,
+  onChange,
+  ...props
+}) {
   return (
     <fieldset className={styles.group}>
       {label && <legend>{label}</legend>}
-      {options.map(opt => (
-        <label key={opt} className={styles.option}>
-          <input type="radio" name={id} value={opt} {...props} />
-          {opt}
-        </label>
-      ))}
+      {options.map(opt => {
+        const optValue = typeof opt === 'string' ? opt : opt.value;
+        const optLabel = typeof opt === 'string' ? opt : opt.label;
+        return (
+          <label key={optValue} className={styles.option}>
+            <input
+              type="radio"
+              name={id}
+              value={optValue}
+              checked={value === optValue}
+              onChange={onChange}
+              {...props}
+            />
+            {optLabel}
+          </label>
+        );
+      })}
     </fieldset>
   );
 }
